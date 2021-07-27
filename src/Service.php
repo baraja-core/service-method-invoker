5a16992b4762362e41d8865ef04ad346d0e6e0b4
Jan Barášek <janbarasek@gmail.com>
2021-07-27 23:10:58 +0200

    Service should implement native Stringable interface.

--- src/Service.php
+++ src/Service.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Baraja;
 
 
-interface Service
+interface Service extends \Stringable
 {
 	/** Return user friendly content or self name. It will be used for exceptions. */
 	public function __toString(): string;
